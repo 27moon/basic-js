ae7f7386d4a8909614c123c2c99c3ad67e069c3e
Marta <emeraldraven27@gmail.com>
2023-12-22 18:17:05 +0300

    feat: add solution for extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function repeater(str, options) {
+//The str and addition parameters are strings by default. 
+//In case when type of these parameters is different, 
+//they must be converted to a string.
+//separator and additionSeparator parameters are strings.
+
+  let repeatTimes = options.repeatTimes || 1;
+  let additionRepeatTimes = options.additionRepeatTimes || 1;
+  let separator = options.separator || '+';
+  let additionSeparator = options.additionSeparator || '|';
+  let addition = options.addition !== undefined ? options.addition : '';
+
+  let additionTotalinOneGo = '';
+
+  for (let i = 1; i < additionRepeatTimes; i++) {
+  additionTotalinOneGo = additionTotalinOneGo + addition + additionSeparator;
+};
+  additionTotalinOneGo = additionTotalinOneGo + addition; //чтобы последняя адишн была без additionSeparator
+// console.log(additionTotalinOneGo)
+
+  let result = '';
+  for (let j = 1; j < repeatTimes; j++) {
+  result = result + str + additionTotalinOneGo + separator;
+  }
+  result = result + str + additionTotalinOneGo; ////чтобы последняя адишн была без separator
+  return result;
+  }
 
 module.exports = {
   repeater
